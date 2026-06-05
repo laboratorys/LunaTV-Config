c6e7d4bd7ae9d3a30ed6ee3dd400e214b2b13414
laboratorys <359916450@qq.com>
2026-06-05 17:01:39 +0800

    update

--- spider.js
+++ spider.js
@@ -6,12 +6,6 @@ const https = require("https");
 const BROWSER_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
     Accept: "*/*",
-    "Accept-Language": "zh-CN,zh;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-    Connection: "keep-alive",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "cross-site",
 };
 const AD_SEGMENT_MAX_COUNT = 20;
 
